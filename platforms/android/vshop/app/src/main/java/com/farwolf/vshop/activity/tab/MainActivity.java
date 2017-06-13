1c16eea4d2fb2dd5732962c14389d6ffaad6d9bb
farwolf2010 <362675035@qq.com>
2017-06-13 21:27:36 +0800

    tab页改成沉侵式

--- platforms/android/vshop/app/src/main/java/com/farwolf/vshop/activity/tab/MainActivity.java
+++ platforms/android/vshop/app/src/main/java/com/farwolf/vshop/activity/tab/MainActivity.java
@@ -4,7 +4,9 @@ package com.farwolf.vshop.activity.tab;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -66,7 +68,13 @@ public class MainActivity extends TabHostActivity {
 //		updateService.init(AppInfo.APP_ID,AppInfo.UPDATE_CHECK_URL);
 //		updateService.doCheck(false,false);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+					| View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+			getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+			getWindow().setStatusBarColor(Color.TRANSPARENT);
+		}
 
 //		show(Classify);
 //		show(Collection);
